0049ab81b66d6698f24ac5e1a4b33848c8a806b3
mnapearson <marratoon@gmail.com>
2021-10-13 23:11:25 +0200

    simple styling of page

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const btn = document.getElementById("button");
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
-  btn.value = "Sending...";
+  btn.value = "sending...";
 
   const serviceID = "default_service";
   const templateID = "template_chulcio";
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
-      btn.value = "Send Email";
-      alert("Sent!");
+      btn.value = "send";
+      alert("Email Sent!");
     },
     (err) => {
-      btn.value = "Send Email";
+      btn.value = "send";
       alert(JSON.stringify(err));
     }
   );
 });
+
+document
+  .getElementById("range-calculator")
+  .scrollIntoView({ behavior: "smooth" });
